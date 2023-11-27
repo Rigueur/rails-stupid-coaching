4f1560870f0c7e7908e87dc7106af8b3f3358991
Steven Despaux <stevendespaux@wanadoo.fr>
2023-11-27 16:12:24 +0100

    coaching done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
     @questions = params[:question]
     if @questions.include? "?"
       @answer = "Silly question, get dressed and go to work!"
-    elsif @questions.include? "I am going to work right now!"
+    elsif @questions.include? "I am going to work right now"
       @answer = "Great!"
     else
       @answer = "I don't care, get dressed and go to work!"
